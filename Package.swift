776ad2198477b82b01acbcafcfb5c54a539a78db
devedbox <devedbox@qq.com>
2018-01-18 10:48:45 +0800

    Updated Package.swift and Package.resolved.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,10 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Carthage/Commandant.git", from: "0.12.0"),
+        .package(url: "https://github.com/jatoben/CommandLine.git", from: "3.0.0-pre1"),
+        .package(url: "https://github.com/nsomar/Swiftline.git", from: "0.5.0"),
+        .package(url: "https://github.com/apple/swift-package-manager.git", from: "0.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
