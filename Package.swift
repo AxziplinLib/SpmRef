b4e98ac951927914bb5dd72a2aa39c4565b875a0
devedbox <devedbox@qq.com>
2018-01-20 11:08:53 +0800

    Added targets.

--- Package.swift
+++ Package.swift
@@ -7,9 +7,9 @@ let package = Package(
     name: "SpmRef",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .executable(
-            name: "SpmRef",
-            targets: ["SpmRef"]),
+        .library(
+            name: "SpmRefKit",
+            targets: ["SpmRefKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,10 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "SpmRef",
+            name: "SpmRefKit",
             dependencies: []),
         .testTarget(
             name: "SpmRefTests",
-            dependencies: ["SpmRef"]),
+            dependencies: ["SpmRefKit"]),
     ]
 )
---
+++ Sources/SpmRef/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SpmRef
+//
+//  Created by devedbox on 2018/1/20.
+//
+
+import Foundation
+
+print("Hello, World!")
+
