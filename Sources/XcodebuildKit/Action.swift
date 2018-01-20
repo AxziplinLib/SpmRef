046628d5a6fa082bec64b0d603f1f094d3753e35
devedbox <devedbox@qq.com>
2018-01-20 18:06:51 +0800

    Renamed with Action.

--- Sources/XcodebuildKit/Action.swift
+++ Sources/XcodebuildKit/Action.swift
@@ -1,14 +1,14 @@
 //
-//  Subcommand.swift
+//  Action.swift
 //  XcodebuildKit
 //
 //  Created by devedbox on 2018/1/20.
 //
 
-// MARK: - Subcommand.
+// MARK: - Action.
 
 extension Xcodebuild {
-    public struct Subcommand: OptionSet {
+    public struct Action: OptionSet {
         /// The element type of the option set.
         public typealias RawValue = UInt
         /// Get the raw value of the option set.
@@ -31,38 +31,38 @@ extension Xcodebuild {
         ///   of `rawValue` potentially represents an element of the option set,
         ///   though raw values may include bits that are not defined as distinct
         ///   values of the `OptionSet` type.
-        public init(rawValue: Xcodebuild.Subcommand.RawValue) {
+        public init(rawValue: Xcodebuild.Action.RawValue) {
             _rawValue = rawValue
         }
         /// Command `build` for building the target in specific workspace or null.
-        public static let build: Subcommand = Subcommand(rawValue: 1 << 1)
+        public static let build: Action = Action(rawValue: 1 << 1)
         /// Command `build-for-testing` for test the targets.
-        public static let buildForTesting: Subcommand = Subcommand(rawValue: 1 << 2)
+        public static let buildForTesting: Action = Action(rawValue: 1 << 2)
         /// Command `analyze` for the static-analyze.
-        public static let analyze: Subcommand = Subcommand(rawValue: 1 << 3)
+        public static let analyze: Action = Action(rawValue: 1 << 3)
         /// Command `archive` for archiving the target to a ipa.
-        public static let archive: Subcommand = Subcommand(rawValue: 1 << 4)
+        public static let archive: Action = Action(rawValue: 1 << 4)
         /// Command `test` for testing the test target.
-        public static let test: Subcommand = Subcommand(rawValue: 1 << 5)
+        public static let test: Action = Action(rawValue: 1 << 5)
         /// Command `test-without-building` for testing the test target without building.
-        public static let testWithoutBuilding: Subcommand = Subcommand(rawValue: 1 << 6)
+        public static let testWithoutBuilding: Action = Action(rawValue: 1 << 6)
         /// Command `install-src` for installing srcs.
-        public static let installSrc: Subcommand = Subcommand(rawValue: 1 << 7)
+        public static let installSrc: Action = Action(rawValue: 1 << 7)
         /// Command `install` for installing.
-        public static let install: Subcommand = Subcommand(rawValue: 1 << 8)
+        public static let install: Action = Action(rawValue: 1 << 8)
         /// Command `clean` for cleaning the targets.
-        public static let clean: Subcommand = Subcommand(rawValue: 1 << 9)
+        public static let clean: Action = Action(rawValue: 1 << 9)
     }
 }
 
-extension Xcodebuild.Subcommand: Hashable {
+extension Xcodebuild.Action: Hashable {
     public var hashValue: Int {
         return Int(_rawValue)
     }
 }
 
 /// All available sub commands for `xcodebuild`.
-private let _availableSubCommandsMap: [Xcodebuild.Subcommand: String] =
+private let _availableActionsMap: [Xcodebuild.Action: String] =
 [.clean : "clean",
  .build : "build",
  .test : "test",
@@ -75,9 +75,9 @@ private let _availableSubCommandsMap: [Xcodebuild.Subcommand: String] =
 
 // MARK: - Commandable Conforming.
 
-extension Xcodebuild.Subcommand: Commandable {
+extension Xcodebuild.Action: Commandable {
     public var command: String {
-        return _availableSubCommandsMap.filter({ self.contains($0.key) }).map({ $0.value }).joined(separator: " ")
+        return _availableActionsMap.filter({ self.contains($0.key) }).map({ $0.value }).joined(separator: " ")
     }
     
     public var description: String {
