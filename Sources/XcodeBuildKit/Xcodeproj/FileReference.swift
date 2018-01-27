275aa24c20c93095e3ff6afdb7a7e0fa00d69326
devedbox <devedbox@qq.com>
2018-01-27 13:40:08 +0800

    Added comments annotations.

--- Sources/XcodeBuildKit/Xcodeproj/FileReference.swift
+++ Sources/XcodeBuildKit/Xcodeproj/FileReference.swift
@@ -12,12 +12,15 @@
 public struct PBXFileReference {
     /// The encoding of the content of the file. Available values defined in `FileEncoding`.
     public var fileEncoding: FileEncoding?
+    
     /// The line ending value of the file. Default is nil.
     public var lineEnding: LineEndings?
     /// The indent width of the file. Default is nil.
     public var indentWidth: UInt?
     /// The tab width if the file. Default is nil.
     public var tabWidth: UInt?
+    /// Should use tabs width instead of intent width. Default is nil.
     public var usesTabs: Bool?
+    /// Should wrap lines of the content of the file.
     public var wrapsLines: Bool?
 }
