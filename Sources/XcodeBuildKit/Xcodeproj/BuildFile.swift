4b31852dea8f12336e21205884e123714abdb5db
devedbox <devedbox@qq.com>
2018-01-28 20:22:30 +0800

    Added BuildFile.swift.

--- Sources/XcodeBuildKit/Xcodeproj/BuildFile.swift
+++ Sources/XcodeBuildKit/Xcodeproj/BuildFile.swift
@@ -5,4 +5,17 @@
 //  Created by devedbox on 2018/1/26.
 //
 
+// MARK: - PBXBuildFile.
 
+/// A type represents the build file of the xcode.
+public struct PBXBuildFile: PBXObject {
+    /// The reference of the original file.
+    public var fileRef: PBXFileReference
+    /// The settings of the build file, such as `COMPILER_FLAGS`.
+    public var settings: [String: Any]?
+}
+
+extension PBXBuildFile {
+    /// The key for the settings of `COMPILER_FLAGS`.
+    public let compilerFlags: String = "COMPILER_FLAGS"
+}
